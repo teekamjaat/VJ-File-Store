c79c8cd7c0faae8bdb5bd0896332d3671509d962
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2024-11-03 19:13:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -96,9 +96,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://aesthetic-saudra-tjbot-72d76ea5.koyeb.app/"
     else:
-        URL = ""
+        URL = "https://aesthetic-saudra-tjbot-72d76ea5.koyeb.app/"
 
 
 
