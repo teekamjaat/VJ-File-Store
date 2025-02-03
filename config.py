b6c99e96919b0bdf0d7fa35139b86137600dff10
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-02-03 13:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "7270727975:AAGsS9QAPwDOpeWAeAtl_6ggwEAVABG
 
 PICS = (environ.get('PICS', 'https://envs.sh/my-.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5469498838').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "TJ_FILE_STORE_BOT") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Hot_Girl_Pornbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
