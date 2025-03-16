8225f1f0dd5a0187baf367c1396670c9e993e55e
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-16 18:35:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,6 +68,8 @@ def formate_file_name(file_name):
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    client = client 
+    massage = massage
     if AUTH_CHANNEL:
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
