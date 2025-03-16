12cc90991dd3ee0b017c67cd8126743d1cd984b0
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-16 11:31:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,6 +16,10 @@ def is_enabled(value, default):
         return False
     else:
         return default
+
+
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
+
       
 # Bot Information
 API_ID = int(environ.get("API_ID", "22349465"))
