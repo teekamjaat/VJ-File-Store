0c97939c12ffe2bc1b9c6dd3975f6b30446a2806
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-16 18:41:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,7 +11,7 @@ from Script import script
 from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
-from pyrogram.errors import 
+from pyrogram.errors import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Massage
 from utils import verify_user, check_token, check_verification, get_token
 from config import AUTH_CHANNEL
