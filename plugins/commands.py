842c892981cbe02f1eb8b0be1825f93e3ba54e4c
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-17 09:27:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,22 +24,24 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-
 async def is_subscribed(bot, query, channel):
     btn = []
+    
+    # Add dynamic channel buttons
     for id in channel:
         chat = await bot.get_chat(int(id))
         try:
             await bot.get_chat_member(id, query.from_user.id)
         except UserNotParticipant:
-            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link])
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
         except Exception as e:
             pass
-    return btn
-           
+
+    # Add two extra fixed channel buttons
     btn.append([InlineKeyboardButton("Join TJ Mod Backup", url="https://t.me/+7TJrDvsrUzZlNmQ1")])
     btn.append([InlineKeyboardButton("Join Main Channel", url="https://t.me/+rwlImG4-g75jMjE1")])
 
+    return btn
 
     
 
