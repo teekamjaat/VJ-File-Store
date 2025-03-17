e12bb3a1f89a779102efac43253269bcce889d16
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-03-17 12:58:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -29,14 +29,13 @@ async def is_subscribed(bot, query, channel):
     btn = []
     for id in channel:
         chat = await bot.get_chat(int(id))
-        btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
-
-    # Additional buttons on separate lines
-    btn.append([InlineKeyboardButton("Join TJ Mod Backup", url="https://t.me/+7TJrDvsrUzZlNmQ1")])
-    btn.append([InlineKeyboardButton("Join Main Channel", url="https://t.me/+rwlImG4-g75jMjE1")])
-
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
     return btn
-
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
